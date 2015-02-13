a6b1c369e1924eac2849d400626cbd02ae8ed782
mficke <matthew.ficke@gmail.com>
2015-02-13 15:29:46 -0500

    dev

--- twitterbot.py
+++ twitterbot.py
@@ -1,36 +1,47 @@
 import twitter
 from local_settings import *
+import database
 
 def connect():
-	api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
-						consumer_secret=MY_CONSUMER_SECRET,
-						access_token_key=MY_ACCESS_TOKEN_KEY,
-						access_token_secret=MY_ACCESS_TOKEN_SECRET)
-	
-	return api
+    api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
+                        consumer_secret=MY_CONSUMER_SECRET,
+                        access_token_key=MY_ACCESS_TOKEN_KEY,
+                        access_token_secret=MY_ACCESS_TOKEN_SECRET)
+    
+    return api
 
-def find_tweets(api, search_query):
-	result = api.GetSearch(term=search_query)
-	
-	for tweet in result:
-		tweet_id = tweet.id
-		tweet_text = tweet.text
-		tweet_user = tweet.user.screen_name
-		break
+def find_tweets(search_query):
+    api = connect()
+    result = api.GetSearch(term=search_query, count=100)
+    previous_tweets = database.read()
+    for tweet in result:
+        if tweet.user.id in previous_tweets: continue
+        if len(tweet.text) > 105: continue
+        tweet_id = tweet.id
+        tweet_text = tweet.text
+        tweet_user = tweet.user.screen_name
+        tweet_user_id = tweet.user.id
+        
+        break
 
-	return tweet_id, tweet_text, tweet_user
+    return tweet_id, tweet_text, tweet_user, tweet_user_id
 
-def post_tweet(api, status_frame, tweet_id, tweet_text, tweet_user):
-	status = status_frame % (tweet_user, tweet_text)
-	
-	api.PostUpdate(status, in_reply_to_status_id=tweet_id)
+def post_tweet(status, tweet_id):
+    api = connect()
+    
+    api.PostUpdate(status, in_reply_to_status_id=tweet_id)
+
+def main():
+    search_query = SEARCH_QUERY
+    status_frame = RESPONSE
+    
+    tweet_id, tweet_text, tweet_user, tweet_user_id = find_tweets(search_query)
+    
+    status = status_frame % (tweet_user, tweet_text)
+    
+    sent_tweet = post_tweet(status, tweet_id)
+    
+    database.write(tweet_user_id)
 
 if __name__ == '__main__':
-	search_query = SEARCH_QUERY
-	status_frame = RESPONSE
-	api = connect()
-	tweet_id, tweet_text, tweet_user = find_tweets(api, search_query)
-	status = status_frame % (tweet_user, tweet_text)
-	
-	sent_tweet = api.PostUpdate(status, in_reply_to_status_id=tweet_id)
-	print sent_tweet.text.encode('utf-8')
+	main()
