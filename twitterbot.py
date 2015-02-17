a1d648f7261f95b7e247f2d51e0018cb1a5e4661
mficke <matthew.ficke@gmail.com>
2015-02-17 12:17:35 -0500

    local_settings import change

--- twitterbot.py
+++ twitterbot.py
@@ -1,13 +1,15 @@
 import twitter
-from local_settings import *
+import local_settings
 import database
 
 def connect():
-    api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
-                        consumer_secret=MY_CONSUMER_SECRET,
-                        access_token_key=MY_ACCESS_TOKEN_KEY,
-                        access_token_secret=MY_ACCESS_TOKEN_SECRET)
-    
+    api = twitter.Api(
+					consumer_key=local_settings.MY_CONSUMER_KEY,
+					consumer_secret=local_settings.MY_CONSUMER_SECRET,
+					access_token_key=local_settings.MY_ACCESS_TOKEN_KEY,
+					access_token_secret=local_settings.MY_ACCESS_TOKEN_SECRET
+					)
+
     return api
 
 def find_tweets(search_query):
@@ -21,26 +23,26 @@ def find_tweets(search_query):
         tweet_text = tweet.text
         tweet_user = tweet.user.screen_name
         tweet_user_id = tweet.user.id
-        
+
         break
 
     return tweet_id, tweet_text, tweet_user, tweet_user_id
 
 def post_tweet(status, tweet_id):
     api = connect()
-    
+
     api.PostUpdate(status, in_reply_to_status_id=tweet_id)
 
 def main():
-    search_query = SEARCH_QUERY
-    status_frame = RESPONSE
-    
+    search_query = local_settings.SEARCH_QUERY
+    status_frame = local_settings.RESPONSE
+
     tweet_id, tweet_text, tweet_user, tweet_user_id = find_tweets(search_query)
-    
+
     status = status_frame % (tweet_user, tweet_text)
-    
+
     sent_tweet = post_tweet(status, tweet_id)
-    
+
     database.write(tweet_user_id)
 
 if __name__ == '__main__':
